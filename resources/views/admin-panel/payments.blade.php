0658c71584d02d380490ce170ee130d109292b9e
Amariah20 <rigodonamariah16@gmail.com>
2024-10-15 09:49:38 +0400

    display payments

---
+++ resources/views/admin-panel/payments.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.admin')
+
+@section('content')
+
+  
+<h1 style="text-align:centre"> Payments </h1>
+<br>
+
+
+@if($errors->any())
+<div class="alert alert-danger">
+    <ul>
+        @foreach($errors->all() as $error)
+        <li>{{ $error}}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
+@if (session('success'))
+    <div class="alert alert-success">
+        {{ session('success') }}
+    </div>
+@endif
+
+
+
+<table class="table table-hover">
+  <thead class="thead-dark">
+    <tr>
+      <th scope="col">Payment ID</th>
+      <th scope="col">Reservation ID</th>
+      <th scope="col">Total Price</th>
+      <th scope="col">Total Paid </th>
+      <th scope="col">Payment Date </th>
+      <th scope="col">Status</th>
+      <th scope="col">Edit</th>
+      
+      
+    </tr>
+  </thead>
+
+  <tbody>
+  @foreach($payments as $payment)
+    <tr>
+      
+      <td>{{$payment->payment_id}}</td>
+      <td> {{$payment->reservation_id}}</td>
+      <td>{{ $payment->reservation ? $payment->reservation->total_price : 'N/A' }}</td>
+      <td>{{ $payment->total_paid }}</td> 
+      <td>{{$payment->payment_date}}</td>
+      <td> {{ $payment->status}}</td>
+
+      
+</tr>
+ @endforeach
+ </tbody>
+ </table>
+
+
+
+@endsection
